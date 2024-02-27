87d37e191cc2ea0ba5f2f381e8068d05f208c476
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2024-02-27 10:40:25 -0300

    Create ex1.java

---
+++ quinto_semestre/java/semana1/ex1.java
@@ -0,0 +1,16 @@
+package ex1;
+import java.util.Scanner;
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+    
+		Scanner entrada = new Scanner(System.in);
+		 int n1 = entrada.nextInt();
+		 int n2 = entrada.nextInt();
+		 entrada.close();
+		 System.out.println(n1 + n2);
+	
+  }
+}
