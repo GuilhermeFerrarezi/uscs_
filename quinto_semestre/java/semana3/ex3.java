04dd28e989b2c0f17f4a4df97a02fa73b51069b1
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2024-03-20 14:56:57 -0300

    Create ex3.java

---
+++ quinto_semestre/java/semana3/ex3.java
@@ -0,0 +1,34 @@
+package uscs;
+
+public class Empregado {
+	
+	private int cod;
+	private String nome;
+	private String cargo;
+	private double salario;
+	private static int qtd;
+	
+	Empregado(int cod, String nome, String cargo, double salario){
+		this.cod = cod;
+		this.nome = nome;
+		this.cargo = cargo;
+		this.salario = salario;
+		AddEmpregado();
+	}
+	
+	Empregado(){
+		AddEmpregado();
+	}
+	
+	void ImprimeEmpregado() {
+		System.out.println("Código do empregado: " + this.cod + "\nNome: " + this.nome + "\ncargo: " + this.cargo + "\nsalário: " + this.salario + "\n");
+	}
+	
+	void AddEmpregado() {
+		qtd++;
+	}
+	
+	static void TotalEmpregados() {
+		System.out.println("Quantidade de empregados: " + qtd);
+	}
+}
