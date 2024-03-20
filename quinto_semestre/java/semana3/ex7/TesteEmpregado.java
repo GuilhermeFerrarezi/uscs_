7eeb7afd05f0c91d673d2d61535815af49258da6
GuilhermeFerrarezi <99054954+GuilhermeFerrarezi@users.noreply.github.com>
2024-03-20 15:20:12 -0300

    Create TesteEmpregado.java

---
+++ quinto_semestre/java/semana3/ex7/TesteEmpregado.java
@@ -0,0 +1,20 @@
+package uscs;
+
+public class TesteEmpregado {
+	
+	public static void main(String[] args) {
+		
+		Empregado[] empregados = new Empregado[2];
+		empregados[0] = new Empregado("Paulo Silva", "Gerente Financeiro", 15400.00);
+		empregados[1] = new Empregado();
+		empregados[1].SetNome("Ana Paula");
+		empregados[1].SetCargo("Analista de Sistemas Senior");
+		empregados[1].SetSalario(107000.00);
+		
+		for(int i = 0; i < Empregado.GetQtd(); i++) {
+			System.out.printf("%dº empregado:\n", i + 1);
+			empregados[i].ImprimeEmpregado();;
+		}
+		Empregado.TotalEmpregados();
+	}
+}
